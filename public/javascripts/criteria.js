866b7eeef5f57a8ff97bf6337452feb51cd8bb3b
naneen <neen_tta@hotmail.com>
2015-04-18 14:44:17 +0700

    created criteria vote page

---
+++ public/javascripts/criteria.js
@@ -0,0 +1,47 @@
+/**
+ * Created by neen_tta on 4/18/15 AD.
+ */
+
+
+// preload image function
+function preload(arrayOfImages) {
+    $(arrayOfImages).each(function(){
+        $('<img/>')[0].src = this;
+    });
+}
+
+$(document).ready(function(){
+
+    // set up radio boxes
+    $('.radioholder').each(function(){
+        $(this).children().hide();
+        var description = $(this).children('label').html();
+        $(this).append('<span class="desc">'+description+'</span>');
+        $(this).prepend('<span class="tick"></span>');
+        // on click, update radio boxes accordingly
+        $(this).click(function(){
+            $(this).children('input').prop('checked', true);
+            $(this).children('input').trigger('change');
+        });
+    });
+    // update radio holder classes when a radio element changes
+    $('input[type=radio]').change(function(){
+        $('input[type=radio]').each(function(){
+            if($(this).prop('checked') == true) {
+                $(this).parent().addClass('activeradioholder');
+            }
+            else $(this).parent().removeClass('activeradioholder');
+        });
+    });
+    // manually fire radio box change event on page load
+    $('input[type=radio]').change();
+
+
+    // preload hover images
+    preload([
+        'http://supereightstudio.com/img/radio_tick.png',
+        'http://supereightstudio.com/img/pulldown.png',
+        'http://supereightstudio.com/img/pulldown_hover.png'
+    ]);
+
+});
